8b960dbeaa0a20ecf6aad666b7fd735f7d438d97
AdrianL99 <55856576+AdrianL99@users.noreply.github.com>
2020-10-28 18:21:31 -0600

    Add files via upload

---
+++ NumerosPseudoaleatorios/src/NumerosPseudoaleatoriosU2/SoftwareGeneradorU2.java
@@ -0,0 +1,18 @@
+
+package NumerosPseudoaleatoriosU2;
+
+import java.io.IOException;
+
+/**
+ *
+ * @author Adrian Leonardo
+ */
+public class SoftwareGeneradorU2 {
+
+    public static void main(String[] args) throws IOException {
+        // TODO code application logic here
+        framePrincipal prueba = new framePrincipal();
+        prueba.setVisible(true);
+    }
+    
+}
